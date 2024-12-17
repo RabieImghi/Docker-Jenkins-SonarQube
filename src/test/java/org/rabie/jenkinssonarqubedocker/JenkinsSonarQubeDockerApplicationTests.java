70d20d5b850fcd208c44f4b27c0d40de8416d04c
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-17 19:30:04 +0100

    test

--- src/test/java/org/rabie/jenkinssonarqubedocker/JenkinsSonarQubeDockerApplicationTests.java
+++ src/test/java/org/rabie/jenkinssonarqubedocker/JenkinsSonarQubeDockerApplicationTests.java
@@ -1,14 +1,14 @@
 package org.rabie.jenkinssonarqubedocker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class JenkinsSonarQubeDockerApplicationTests {
 
     @Test
     void getTest() {
-        System.out.println("Test");
+        assertTrue(true);
     }
 
 
